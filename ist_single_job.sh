1e1b46d109b53856ccf3a2e4e75f642cdc2f45ca
shaswot-utokyo <shaswot@g.ecc.u-tokyo.ac.jp>
2020-02-19 16:33:20 +0900

    ist_sync

--- ist_single_job.sh
+++ ist_single_job.sh
@@ -9,10 +9,11 @@
 
 # use partition 'p'
 ##SBATCH -p p
-#SBATCH -p v
+##SBATCH -p v
+#SBATCH -p knm
 
 # use gpu resource
-#SBATCH  --gres=gpu:1
+##SBATCH  --gres=gpu:1
 
 # allocate run time
 #SBATCH --time=10:00:00
@@ -72,8 +73,7 @@ experiment=$2
 
 # IST
 # # SRG = 0.0001
-python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=1 --srg=0.0001 $2
-# IST PENDING
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=1 --srg=0.0001 $2
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=2 --srg=0.0001 $2
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=3 --srg=0.0001 $2
 
@@ -88,6 +88,7 @@ python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=1 --srg=0.
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=1 --double --dueling --srg=0.0001 $2
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=2 --double --dueling --srg=0.0001 $2
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=$1 --nsteps=3 --double --dueling --srg=0.0001 $2
+#>>>>>
 
 # REED
 # # SRG = 0.0002

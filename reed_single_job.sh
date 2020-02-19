aa39395463f7647a468bfb0c0fe1ce0900356944
5mali <shaswot@gmail.com>
2020-02-19 16:13:05 +0900

    reed_single_job updated

--- reed_single_job.sh
+++ reed_single_job.sh
@@ -23,21 +23,28 @@ export PATH=$PYTHONUSERBASE/bin:$PATH
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=<seed_value> --nsteps=<rollout_length> --double --dueling --srg=<srg_ratio> <experiment>
 
 # # SRG = OFF
+# REED
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 ${experiment}
 
-#python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 ${experiment}
-#python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 ${experiment}
-
-#Pending
-python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 ${experiment}
-
+# IST
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double ${experiment}
 
+# REED 
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --dueling ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --dueling ${experiment}
+#>>
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --dueling ${experiment}
+
+# IST
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --dueling ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --dueling ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --dueling ${experiment}
 
+# IST
 # # SRG = 0.0001
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --srg=0.0001 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --srg=0.0001 ${experiment}
@@ -47,10 +54,15 @@ python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 ${e
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --srg=0.0001 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --srg=0.0001 ${experiment}
 
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --dueling --srg=0.0001 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --dueling --srg=0.0001 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --dueling --srg=0.0001 ${experiment}
+
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --dueling --srg=0.0001 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --dueling --srg=0.0001 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --dueling --srg=0.0001 ${experiment}
 
+# REED
 # # SRG = 0.0002
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --srg=0.0002 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --srg=0.0002 ${experiment}
@@ -60,10 +72,15 @@ python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 ${e
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --srg=0.0002 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --srg=0.0002 ${experiment}
 
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --dueling --srg=0.0002 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --dueling --srg=0.0002 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --dueling --srg=0.0002 ${experiment}
+
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --dueling --srg=0.0002 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --dueling --srg=0.0002 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --dueling --srg=0.0002 ${experiment}
 
+# IST
 # # SRG = 0.0005
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --srg=0.0005 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --srg=0.0005 ${experiment}
@@ -72,6 +89,10 @@ python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 ${e
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --srg=0.0005 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --srg=0.0005 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --srg=0.0005 ${experiment}
+
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --dueling --srg=0.0005 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --dueling --srg=0.0005 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --dueling --srg=0.0005 ${experiment}
 
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --dueling --srg=0.0005 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --dueling --srg=0.0005 ${experiment}
@@ -85,6 +106,10 @@ python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 ${e
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --srg=0.0008 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --srg=0.0008 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --double --srg=0.0008 ${experiment}
+
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --dueling --srg=0.0008 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --dueling --srg=0.0008 ${experiment}
+# python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=3 --dueling --srg=0.0008 ${experiment}
 
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=1 --double --dueling --srg=0.0008 ${experiment}
 # python dqn_nstep_double_dueling_srgratio.py --cuda --seed=${seed} --nsteps=2 --double --dueling --srg=0.0008 ${experiment}
